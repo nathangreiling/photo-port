a8c1ac3924594b94527e7d62b8d9aaa367bb08aa
nathangreiling <ngreiling20@gmail.com>
2022-06-21 18:02:33 -0500

    done

--- photo-port/src/components/Gallery/index.js
+++ photo-port/src/components/Gallery/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { capitalizeFirstLetter } from '../../utils/helpers'
 import PhotoList from "../Photolist/Photolist";
 
-    function Gallery({ currentCategory }) {
-        const { name, description } = currentCategory;
+    function Gallery(props) {
+        const { currentCategory } = props;
         return (
           <section>
-            <h1 data-testid="h1tag">{capitalizeFirstLetter(name)}</h1>
-            <p>{description}</p>
-            <PhotoList  />
+            <h1 data-testid="h1tag">{capitalizeFirstLetter(currentCategory.name)}</h1>
+            <p>{currentCategory.description}</p>
+            <PhotoList category={currentCategory.name}  />
           </section>
         );
       }
